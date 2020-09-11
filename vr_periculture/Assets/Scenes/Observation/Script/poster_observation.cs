f6cf6c8ceec4618a1797c52a24e04e382355ad96
MADBOU <32713204+MADBOU@users.noreply.github.com>
2020-09-11 15:26:17 +0200

    V0.03
    deroulement des teleportation

---
+++ vr_periculture/Assets/Scenes/Observation/Script/poster_observation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class poster_observation : MonoBehaviour
+{
+    public GameObject next_point;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "PlayerController")
+        {
+            
+            StartCoroutine(coroutine_next());
+        }
+    }
+    IEnumerator coroutine_next()
+    {
+        Debug.Log("coroutine  B created");
+        yield return new WaitForSeconds(5f);
+        next_point.SetActive(true);
+    }
+}
---
+++ vr_periculture/Assets/Scenes/Observation/Script/women_walking.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class women_walking : MonoBehaviour
+{
+    public GameObject next_point;
+    public GameObject women;
+    Animator animator;
+    UnityStandardAssets.Characters.ThirdPerson.AI_Worker_Walking AI_Worker_Walking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = women.GetComponent<Animator>();
+        AI_Worker_Walking = women.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AI_Worker_Walking>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "PlayerController")
+        {
+            women.SetActive(true);
+            animator.SetBool("IsWalking", true);
+            AI_Worker_Walking.isWalking = true;
+            StartCoroutine(coroutine_next());
+            StartCoroutine(coroutine_stopanimation());
+        }
+    }
+    IEnumerator coroutine_next()
+    {
+        Debug.Log("coroutine  women created");
+        yield return new WaitForSeconds(5f);
+        next_point.SetActive(true);
+    }
+    IEnumerator coroutine_stopanimation()
+    {
+        Debug.Log("coroutine  stop created");
+        yield return new WaitForSeconds(14.5f);
+        animator.SetBool("IsWalking", false);
+        AI_Worker_Walking.isWalking = false;
+    }
+}
