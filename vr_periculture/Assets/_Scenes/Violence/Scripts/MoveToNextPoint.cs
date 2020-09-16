769d4303cb2cdf973acdc3c58489b5c4af86cb40
AndreAroCiVR <45397854+AndreAroCiVR@users.noreply.github.com>
2020-09-16 11:04:31 +0200

    Violence 8
    Canvas de réponse added and coded

---
+++ vr_periculture/Assets/_Scenes/Violence/Scripts/MoveToNextPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToNextPoint : MonoBehaviour
+{
+    public GameObject next_point;
+    private void Start()
+    {
+        
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PlayerController")
+        {
+
+            StartCoroutine(Coroutine_next());
+
+        }
+    }
+    IEnumerator Coroutine_next()
+    {
+        Debug.Log("coroutine  women created");
+        yield return new WaitForSeconds(5f);
+        next_point.SetActive(true);
+    }
+}
