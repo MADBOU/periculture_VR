a53290fcb5efe2f0bb9a0c90e1cb76ec9b710e6a
AndreAroCiVR <45397854+AndreAroCiVR@users.noreply.github.com>
2020-09-22 10:49:14 +0200

    NE PRENDS PAS
    Oblligié de pusher pour pouvoir faire Pull Origin

--- vr_periculture/Assets/_Scenes/Violence/Scripts/MoveToNextPoint.cs
+++ vr_periculture/Assets/_Scenes/Violence/Scripts/MoveToNextPoint.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class MoveToNextPoint : MonoBehaviour
 {
     public GameObject next_point;
+    public int risksToComplete;
+    private int counter;
     private void Start()
     {
-        
+        counter = 0;
+        Debug.Log("Start MoveToNextPoint");
+        Debug.Log("Counter in Start : " + counter);
     }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.name == "PlayerController")
         {
-
-            StartCoroutine(Coroutine_next());
-
+            this.gameObject.GetComponent<CapsuleCollider>().isTrigger = true;
         }
     }
-    IEnumerator Coroutine_next()
+    //IEnumerator Coroutine_next()
+    //{
+    //    yield return new WaitForSeconds(5f);
+    //    next_point.SetActive(true);
+    //}
+
+    public void CheckAndActivateNextCheckPoint()
     {
-        Debug.Log("coroutine  women created");
-        yield return new WaitForSeconds(5f);
-        next_point.SetActive(true);
+        counter++;
+        Debug.Log("Counter: " + counter);
+        if(risksToComplete == counter)
+        {
+            this.gameObject.SetActive(false);
+            next_point.SetActive(true);
+            //counter = 0;
+            Debug.Log("CheckPoint name: " +next_point.gameObject.name);
+            Debug.Log("CheckPoint Activated: ");
+        }
     }
 }
